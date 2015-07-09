2faa11864c7dd0a54255959d0779cbe39b6021ef
Paul Hilbert <me@sicgraph.org>
2015-07-09 16:09:03 +0200

    added docker build bootstrap files

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM paulhilbert/wp4_wp5_base:latest
+MAINTAINER Richard Vock <vock@cs.uni-bonn.de>
+ADD docker_build.sh /tmp/build.sh
+RUN /bin/sh /tmp/build.sh ifc_mesh_extract
---
+++ docker_build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+PROJECT="$1"
+
+cd /tmp
+git clone https://github.com/paulhilbert/${PROJECT}.git
+mkdir -p ${PROJECT}/build
+cd ${PROJECT}/build
+cmake -D CMAKE_INSTALL_PREFIX="/usr" -D CMAKE_BUILD_TYPE="Release" ..
+make
+make install
+cd /tmp
+rm -rf ${PROJECT}
